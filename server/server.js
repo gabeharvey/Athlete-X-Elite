f18ca7f38506a37af6c77d1850f55dfaad657ff9
Gabe Harvey <harvey7@gmail.com>
2024-11-06 10:53:38 -0600

    Updated CORS Settings in server.js

--- server/server.js
+++ server/server.js
@@ -22,7 +22,7 @@ const __dirname = path.dirname(__filename);
 
 const allowedOrigins = [
   'http://localhost:5173',
-  'https://athletexelite.onrender.com',
+  'https://athletexelite.onrender.com/',
 ];
 
 app.use(cors({
