f5121e8340d4837ec18a4dd7a53dc4f33b6981dd
Gabe Harvey <harvey7@gmail.com>
2024-11-10 13:49:00 -0600

    Updated Scripts in Server package.json

--- server/server.js
+++ server/server.js
@@ -135,6 +135,8 @@ app.get('*', (req, res) => {
   res.sendFile(path.join(__dirname, 'client', 'build', 'index.html'));
 });
 
+app.get('/favicon.ico', (req, res) => res.sendFile(path.join(__dirname, 'public', 'favicon.ico')));
+
 app.use((err, req, res, next) => {
   console.error(err.stack);
   res.status(500).send('Something went wrong!');
